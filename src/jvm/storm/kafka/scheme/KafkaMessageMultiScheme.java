14ec70aa269851bf280af33dfb5e69454cb044b2
jblakeney <jblakeney@enernoc.com>
2014-04-21 13:06:40 -0400

    Whitespace/code formatting fix

--- src/jvm/storm/kafka/scheme/KafkaMessageMultiScheme.java
+++ src/jvm/storm/kafka/scheme/KafkaMessageMultiScheme.java
@@ -6,18 +6,21 @@ import java.util.Arrays;
 import java.util.List;
 
 public class KafkaMessageMultiScheme extends SchemeAsMultiScheme {
-	
-	// Store copy of scheme to avoid need for casting
-	private KafkaMessageScheme kmScheme;
-	
-	public KafkaMessageMultiScheme(KafkaMessageScheme scheme) {
-		super(scheme);
-		kmScheme = scheme;
-	}
-	
-	public Iterable<List<Object>> deserialize(final Message message) {
+
+    // Store copy of scheme to avoid need for casting
+    private KafkaMessageScheme kmScheme;
+
+    public KafkaMessageMultiScheme(KafkaMessageScheme scheme) {
+        super(scheme);
+        kmScheme = scheme;
+    }
+
+    public Iterable<List<Object>> deserialize(final Message message) {
         List<Object> o = kmScheme.deserialize(message);
-        if(o == null) return null;
-        else return Arrays.asList(o);
+        if (o == null) {
+            return null;
+        } else {
+            return Arrays.asList(o);
+        }
     }
 }
--- src/jvm/storm/kafka/scheme/KafkaMessageScheme.java
+++ src/jvm/storm/kafka/scheme/KafkaMessageScheme.java
@@ -12,29 +12,28 @@ import java.io.UnsupportedEncodingException;
 
 public class KafkaMessageScheme implements Scheme {
 
-	@Override
-	public List<Object> deserialize(byte[] ser) {
-		return new Values(deserializeString(ser));
-	}
+    @Override
+    public List<Object> deserialize(byte[] ser) {
+        return new Values(deserializeString(ser));
+    }
 
-	@Override
+    @Override
     public Fields getOutputFields() {
         return new Fields("key", "payload");
     }
-	
-	public List<Object> deserialize(Message message) {
-		List<Object> deser = new ArrayList<Object>();
-		ByteBuffer key = message.key();
+
+    public List<Object> deserialize(Message message) {
+        List<Object> deser = new ArrayList<Object>();
+        ByteBuffer key = message.key();
         if (key == null) {
-        	deser.add(null);            
-        }
-        else {
-        	deser.addAll(deserialize(Utils.toByteArray(key)));
+            deser.add(null);
+        } else {
+            deser.addAll(deserialize(Utils.toByteArray(key)));
         }
         deser.addAll(deserialize(Utils.toByteArray(message.payload())));
         return deser;
     }
-    
+
     private static String deserializeString(byte[] string) {
         try {
             return new String(string, "UTF-8");
